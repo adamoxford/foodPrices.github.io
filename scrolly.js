2def7bc31a84deb5539733c170c55f9ba3762feb
Adam <adam@adamoxford.co.uk>
2025-11-20 10:45:21 +0200

    add sixth chart

--- scrolly.js
+++ scrolly.js
@@ -6,7 +6,8 @@ const chartSpecs = [
     'chart_state_2.json',  // Corresponds to data-step="1"
     'chart_state_3.json',
     'chart_state_4.json',
-    'chart_state_5.json'
+    'chart_state_5.json',
+    'chart_state_6.json
 ];
 
 const visElement = document.getElementById('vis');
